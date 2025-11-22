117818f40c1001231cb7a862137040fb42778ffd
mudit0810 <79301841+mudit0810@users.noreply.github.com>
2025-11-22 18:05:17 -0500

    secondupdatedAppfile

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("This is the App class.");
+        System.out.println("Now calling secondfile...");
         // Call the main method of secondfile
         secondfile.main(args);
     }
